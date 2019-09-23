7db2e860bb302cb86a91d328f20029ed74e54df2
mzp23 <mzgme23@gmail.com>
2019-09-23 08:41:15 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+      const re = /(\|\|)|(\(\))|(\{\}|(\[\]))|12|34|56|77|88/;
+      while (re.test(str)) {
+        str = str.replace(re, "");
+      }        
+      return !str; 
 }
